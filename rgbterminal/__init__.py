8007a34b09b7b9e0dc3124d541311f6a4dd761bd
cvcvka5 <cvcvka5@gmail.com>
2025-11-05 14:42:42 +0100

    GradientText, GradientTextFactory

--- rgbterminal/__init__.py
+++ rgbterminal/__init__.py
@@ -1,4 +1,4 @@
 # rgbtext/__init__.py
-from .core import RGBText, RGBTextFactory
+from .core import RGBText, RGBTextFactory, GradientText, GradientTextFactory
 
-__all__ = ["RGBText", "RGBTextFactory"]
+__all__ = ["RGBText", "RGBTextFactory", "GradientText", "GradientTextFactory"]
